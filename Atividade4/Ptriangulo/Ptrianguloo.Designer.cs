01121df7559912e733917dc5c4a5ac7408f4ce53
LeonardoHyppolito <94125626+LeonardoHyppolito@users.noreply.github.com>
2023-03-27 22:18:13 -0300

    Add files via upload

---
+++ Atividade4/Ptriangulo/Ptrianguloo.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace Ptriangulo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnForm_Tri = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.txtLadoA = new System.Windows.Forms.TextBox();
+            this.txtLadoB = new System.Windows.Forms.TextBox();
+            this.txtLadoC = new System.Windows.Forms.TextBox();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.txtTipoTri = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(403, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lado A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(403, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lado B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(403, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Lado C";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(377, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Tipo do Triângulo ";
+            // 
+            // btnForm_Tri
+            // 
+            this.btnForm_Tri.Location = new System.Drawing.Point(380, 213);
+            this.btnForm_Tri.Name = "btnForm_Tri";
+            this.btnForm_Tri.Size = new System.Drawing.Size(145, 30);
+            this.btnForm_Tri.TabIndex = 4;
+            this.btnForm_Tri.Text = "Formar Triângulo ";
+            this.btnForm_Tri.UseVisualStyleBackColor = true;
+            this.btnForm_Tri.Click += new System.EventHandler(this.btnForm_Tri_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(575, 218);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(156, 24);
+            this.btnLimpar.TabIndex = 5;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // txtLadoA
+            // 
+            this.txtLadoA.Location = new System.Drawing.Point(547, 42);
+            this.txtLadoA.Name = "txtLadoA";
+            this.txtLadoA.Size = new System.Drawing.Size(202, 20);
+            this.txtLadoA.TabIndex = 6;
+            this.txtLadoA.Validated += new System.EventHandler(this.txtLadoA_Validated);
+            // 
+            // txtLadoB
+            // 
+            this.txtLadoB.Location = new System.Drawing.Point(547, 91);
+            this.txtLadoB.Name = "txtLadoB";
+            this.txtLadoB.Size = new System.Drawing.Size(202, 20);
+            this.txtLadoB.TabIndex = 7;
+            this.txtLadoB.Validated += new System.EventHandler(this.txtLadoB_Validated);
+            // 
+            // txtLadoC
+            // 
+            this.txtLadoC.Location = new System.Drawing.Point(547, 137);
+            this.txtLadoC.Name = "txtLadoC";
+            this.txtLadoC.Size = new System.Drawing.Size(202, 20);
+            this.txtLadoC.TabIndex = 8;
+            this.txtLadoC.TextChanged += new System.EventHandler(this.txtLadoC_TextChanged);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(575, 248);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(156, 26);
+            this.btnFechar.TabIndex = 9;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // txtTipoTri
+            // 
+            this.txtTipoTri.Location = new System.Drawing.Point(545, 182);
+            this.txtTipoTri.Name = "txtTipoTri";
+            this.txtTipoTri.Size = new System.Drawing.Size(204, 20);
+            this.txtTipoTri.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtTipoTri);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.txtLadoC);
+            this.Controls.Add(this.txtLadoB);
+            this.Controls.Add(this.txtLadoA);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnForm_Tri);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnForm_Tri;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.TextBox txtLadoA;
+        private System.Windows.Forms.TextBox txtLadoB;
+        private System.Windows.Forms.TextBox txtLadoC;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.TextBox txtTipoTri;
+    }
+}
+
---
+++ Atividade4/Ptriangulo/Ptrianguloo.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ptriangulo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtLadoA_Validated(object sender, EventArgs e)
+        { double ladoA;
+
+            if (!double.TryParse(txtLadoA.Text, out ladoA))
+                _ = MessageBox.Show("Lado A precisa ser um número");
+            //= txtLadoA.Focus();
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Close(); 
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtLadoA.Clear();
+            txtLadoB.Clear();
+            txtLadoC.Clear();
+            txtTipoTri.Clear();
+        }
+
+        private void txtLadoB_Validated(object sender, EventArgs e)
+        {
+            double ladoB;
+            if (!double.TryParse(txtLadoB.Text, out ladoB))
+                _ = MessageBox.Show("Lado B precisa ser um número");
+             //= txtLadoB.Focus();
+
+        }
+
+        private void txtLadoC_TextChanged(object sender, EventArgs e)
+        {
+            double ladoC;
+            if (!double.TryParse(txtLadoC.Text, out ladoC))
+                _ = MessageBox.Show("Lado C precisa ser um número");
+            //_ = txtLadoA.Focus();
+        }
+
+        private void btnForm_Tri_Click(object sender, EventArgs e)
+        {
+            double numA, numB, numC, dif1, dif2, dif3;
+            numA = double.Parse(txtLadoA.Text);
+            numB = double.Parse(txtLadoB.Text);
+            numC = double.Parse(txtLadoC.Text);
+            dif1 = Math.Abs(numA - numB);
+            dif2 = Math.Abs(numA - numC);
+            dif3 = Math.Abs(numB - numC);
+            if(numA < numB + numC) && (numB < numA + numC) && (numC < numA + numB)
+                    // tentei de tudo, não consegui. 
+
+
+             
+        }   
+    }
+
